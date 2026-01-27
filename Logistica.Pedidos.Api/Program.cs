96ebdce981589925af4bf5acb88d84547f2ef242
rafaelcunhaa <rafaeldacunha.9615@gmail.com>
2026-01-27 11:38:42 -0300

    feat(pedidos): persistencia com EF Core e SQL Server (docker)

--- Logistica.Pedidos.Api/Program.cs
+++ Logistica.Pedidos.Api/Program.cs
@@ -11,6 +11,11 @@ using Logistica.Pedidos.Api.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configura o EF Core para usar SQL Server (container Docker)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
